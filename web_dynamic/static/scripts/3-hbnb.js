469387d6a795a1a18ae37fce154b29e07ff2ae49
LJThao <yengfong.thao@atlasschool.com>
2024-08-06 19:32:38 +0000

    task 4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    const HOST = 'localhost';
+    const amenityObj = {};
+
+    $('.amenities .popover input').change(function () {
+        const key = $(this).data('name');
+        const id = $(this).data('id');
+        if ($(this).is(':checked')) {
+            amenityObj[key] = id;
+        } else {
+            delete amenityObj[key];
+        }
+        $('.amenities h4').text(Object.keys(amenityObj).sort().join(', '));
+    });
+
+    function apiStatus() {
+        $.get(`http://${HOST}:5001/api/v1/status/`, (data) => {
+            $('#api_status').toggleClass('available', data.status === 'OK');
+        });
+    }
+
+    function fetchPlaces() {
+        $.ajax({
+            url: `http://${HOST}:5001/api/v1/places_search/`,
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({}),
+            success: function(response) {
+                $('section.places').empty();
+                response.forEach(place => {
+                    $('section.places').append(`
+                        <article>
+                            <div class="title_box">
+                                <h2>${place.name}</h2>
+                                <div class="price_by_night">$${place.price_by_night}</div>
+                            </div>
+                            <div class="information">
+                                <div class="max_guest">${place.max_guest} Guest(s)</div>
+                                <div class="number_rooms">${place.number_rooms} Bedroom(s)</div>
+                                <div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)</div>
+                            </div>
+                            <div class="description">${place.description.replace(/Owner:.*$/, '')}</div>
+                        </article>`);
+                });
+            },
+            error: function(error) {
+                console.error('Error fetching places:', error);
+            }
+        });
+    }
+
+    apiStatus();
+    fetchPlaces();
+});
